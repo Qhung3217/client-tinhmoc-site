ea0ba61d3a00a5380628b990d43b81aa3106de7f
Qhung3217 <quanghung165200@gmail.com>
2024-07-22 17:30:56 +0700

    add custom modal

---
+++ src/components/custom-modal/custom-modal.tsx
@@ -0,0 +1,46 @@
+import type { ModalProps } from '@mui/material';
+
+import { Box, Modal, IconButton } from '@mui/material';
+
+import { Iconify } from '../iconify';
+
+type Props = ModalProps;
+export default function CustomModal({ children, sx, ...rest }: Props) {
+  return (
+    <Modal disableAutoFocus {...rest}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          maxWidth: 'md',
+          width: '90%',
+          bgcolor: 'transparent',
+
+          ...sx,
+        }}
+      >
+        <IconButton
+          sx={{
+            ml: 'auto',
+            mr: 0,
+            display: 'flex',
+            '&:hover': {
+              color: 'white',
+            },
+          }}
+        >
+          <Iconify icon="rivet-icons:close" />
+        </IconButton>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+          }}
+        >
+          {children}
+        </Box>
+      </Box>
+    </Modal>
+  );
+}
