403b19794058c25685707989af0e2843447c0a66
Qhung3217 <quanghung165200@gmail.com>
2024-07-24 15:14:06 +0700

    [done] door carousel

--- src/sections/@landing/home/home-doors.tsx
+++ src/sections/@landing/home/home-doors.tsx
@@ -1,56 +1,59 @@
-import { useRef, useEffect } from 'react';
+import { useState } from 'react';
+import { config } from 'react-spring';
+import Carousel from 'react-spring-3d-carousel';
 
 import { Box } from '@mui/material';
 
-import { Image } from 'src/components/image';
-import { useCarousel } from 'src/components/carousel';
+import { uuidv4 } from 'src/utils/uuidv4';
 
-const slides = [
-  {
-    title: '1L01',
-    url: '/assets/landing/doors/1L01.jpg',
-  },
-  {
-    title: '2C02-G',
-    url: '/assets/landing/doors/2C02-G.jpg',
-  },
-  {
-    title: '2C02',
-    url: '/assets/landing/doors/2C02.jpg',
-  },
-  {
-    title: '2L01',
-    url: '/assets/landing/doors/2L01.jpg',
-  },
-  {
-    title: '2S01',
-    url: '/assets/landing/doors/2S01.jpg',
-  },
-  {
-    title: '3C02',
-    url: '/assets/landing/doors/3C02.jpg',
-  },
-  {
-    title: '3S02',
-    url: '/assets/landing/doors/3S02.jpg',
-  },
-  {
-    title: '5L01',
-    url: '/assets/landing/doors/5L01.jpg',
-  },
-];
+import { SubTitle, SectionTitle } from '../_common/section-title';
 
 export default function HomeDoors() {
-  const carousel = useCarousel({ loop: true, slidesToShow: 7, align: 'center' });
-  const ref = useRef<any>();
-  useEffect(() => {
-    window.$(window);
-    console.log(ref.current, $('#carousel'));
-    ref.current?.waterwheelCarousel();
-  }, [ref]);
+  const [nextSlide, setNextSlide] = useState(0);
+  const slides = [
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/1L01.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/2C02-G.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/2C02.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/2L01.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/2S01.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/3C02.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/3S02.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <img alt="" src="/assets/landing/doors/5L01.jpg" />,
+    },
+  ].map((slide, index) => ({ ...slide, onClick: () => setNextSlide(index) }));
+
   return (
-    <Box component="section">
-      <Box position="relative">
+    <Box
+      component="section"
+      sx={{
+        pt: 25,
+        backgroundColor: '#1a1a1a',
+      }}
+    >
+      <Box position="relative" width={673} height={600} mx="auto">
         {/* <Carousel carousel={carousel}>
           {slides.map((slide) => (
             <Box sx={{ position: 'relative' }} key={slide.title}>
@@ -58,13 +61,26 @@ export default function HomeDoors() {
             </Box>
           ))}
         </Carousel> */}
-        <Box component="div" id="carousel" ref={ref}>
-          {slides.map((slide, index) => (
-            <Box sx={{ position: 'relative' }} key={slide.title} id={`item-${index + 1}`}>
-              <Image visibleByDefault alt={slide.title} src={slide.url} />
-            </Box>
-          ))}
-        </Box>
+        <Carousel
+          slides={slides}
+          goToSlide={nextSlide}
+          showNavigation={false}
+          animationConfig={config.stiff}
+          offsetRadius={3}
+        />
+      </Box>
+      <Box
+        sx={{
+          py: 7.5,
+          px: 2.5,
+          pb: 13.75,
+        }}
+      >
+        <SectionTitle>KIỂU DÁNG</SectionTitle>
+        <SubTitle>
+          Có nhiều kiểu dáng để chọn <br /> Có hoạ sĩ thiết kế theo mẫu riêng <br />& có cả một đội
+          ngũ phục vụ quý khách
+        </SubTitle>
       </Box>
     </Box>
   );
