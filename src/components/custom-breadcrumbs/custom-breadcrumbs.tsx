ba1ecc4fdaf910de009f7f796dc52c78a6e1e753
Qhung3217 <quanghung165200@gmail.com>
2024-07-30 10:51:13 +0700

    update custom breadcrumbs component

--- src/components/custom-breadcrumbs/custom-breadcrumbs.tsx
+++ src/components/custom-breadcrumbs/custom-breadcrumbs.tsx
@@ -76,14 +76,8 @@ export function CustomBreadcrumbs({
 
 function Separator() {
   return (
-    <Box
-      component="span"
-      sx={{
-        width: 4,
-        height: 4,
-        borderRadius: '50%',
-        bgcolor: 'text.disabled',
-      }}
-    />
+    <Box component="span" sx={{}}>
+      /
+    </Box>
   );
 }
