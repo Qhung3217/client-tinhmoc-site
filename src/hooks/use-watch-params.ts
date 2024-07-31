52467fb03d14392835f76074fefb4f93c1af317d
Qhung3217 <quanghung165200@gmail.com>
2024-07-31 17:35:18 +0700

    add use watch params hook

---
+++ src/hooks/use-watch-params.ts
@@ -0,0 +1,33 @@
+import { useMemo, useState } from 'react';
+import { isEqual, cloneDeep } from 'lodash';
+
+import { useSearchParams } from 'src/routes/hooks';
+
+export default function useWatchParams(params: string | string[]) {
+  const searchParams = useSearchParams();
+
+  const [state, setState] = useState<{
+    [key: string]: string | null;
+  }>(() => {
+    if (Array.isArray(params)) {
+      return params.reduce((obj, key) => ({ ...obj, [key]: '' }), {});
+    }
+    return {
+      [params]: '',
+    };
+  });
+
+  const memoizedValue = useMemo(() => {
+    const keys = Object.keys(state);
+    const newObj = cloneDeep(state);
+    keys.forEach((key) => {
+      newObj[key] = searchParams.get(key);
+    });
+    if (!isEqual(state, newObj)) {
+      return newObj;
+    }
+    return state;
+  }, [state, searchParams]);
+
+  return memoizedValue;
+}
